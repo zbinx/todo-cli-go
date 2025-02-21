006eb1a37656f867cc449928144090cf77bc9a53
h4ckery.b1nx <51971090+zbinx@users.noreply.github.com>
2025-02-20 23:14:02 -0500

    Update main.go , fixed spaces

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
     "fmt"
     "os"
-	"strconv"
-	"encoding/json"
+    "strconv"
+    "encoding/json"
 )
 
 type Task struct {
-	ID int `json: "id"`
-	Content string `json:"content"`
-	Done bool `json:"done"`
+    ID int `json: "id"`
+    Content string `json:"content"`
+    Done bool `json:"done"`
 }
 
 const taskFile = "taskFile.json"
@@ -19,130 +19,130 @@ var taskList []Task
 
 func main() {
 
-	loadTaskFile()
+    loadTaskFile()
 
     if len(os.Args) < 2 {
-		fmt.Println("Usage: todo <command> [arguments]")
-		return
-	}
+        fmt.Println("Usage: todo <command> [arguments]")
+        return
+    }
 
-	command := os.Args[1]
+    command := os.Args[1]
 
-	switch command {
-	case "add":
-		addTask()
+    switch command {
+    case "add":
+        addTask()
 
-	case "list":
-		listTask()
+    case "list":
+        listTask()
 
-	case "done":
-		markTaskAsDone()
+    case "done":
+        markTaskAsDone()
 
-	default:
-		fmt.Println("Unknown command. Available commands: add, list, done")
+    default:
+        fmt.Println("Unknown command. Available commands: add, list, done")
 
-	}
+    }
 
 }
 
 func loadTaskFile() {
-	file, err := os.Open(taskFile)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return
-		}
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
+    file, err := os.Open(taskFile)
+    if err != nil {
+        if os.IsNotExist(err) {
+            return
+        }
+        fmt.Println("Error opening file:", err)
+        return
+    }
+    defer file.Close()
 
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(&taskList); err != nil {
-		fmt.Println("Error decoding JSON:", err)
-	}
+    decoder := json.NewDecoder(file)
+    if err := decoder.Decode(&taskList); err != nil {
+        fmt.Println("Error decoding JSON:", err)
+    }
 }
 
 func saveTasks() {
-	file, err := os.Create(taskFile)
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
+    file, err := os.Create(taskFile)
+    if err != nil {
+        fmt.Println("Error creating file:", err)
+        return
+    }
+    defer file.Close()
 
-	encoder := json.NewEncoder(file)
-	if err := encoder.Encode(taskList); err != nil {
-		fmt.Println("Error writing to file:", err)
-	} else {
-		fmt.Println("Tasks successfully saved to", taskFile)
-	}
+    encoder := json.NewEncoder(file)
+    if err := encoder.Encode(taskList); err != nil {
+        fmt.Println("Error writing to file:", err)
+    } else {
+        fmt.Println("Tasks successfully saved to", taskFile)
+    }
 }
 
 func addTask() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: todo <command> [arguments]")
-		return
-	}
+    if len(os.Args) < 3 {
+        fmt.Println("Usage: todo <command> [arguments]")
+        return
+    }
 
-	taskContent := os.Args[2]
+    taskContent := os.Args[2]
 
-	newTask := Task{
-		ID: len(taskList) + 1,
-		Content: taskContent,
-		Done: false, 	
-	}
+    newTask := Task{
+        ID: len(taskList) + 1,
+        Content: taskContent,
+        Done: false, 	
+    }
 
-	taskList = append(taskList, newTask)
-	saveTasks()
+    taskList = append(taskList, newTask)
+    saveTasks()
 
-	fmt.Printf("Task added: %s\n", newTask.Content)
+    fmt.Printf("Task added: %s\n", newTask.Content)
 }
 
 func listTask() {
-	if len(taskList) == 0 {
-		fmt.Println("No tasks available.")
-		return
-	}
+    if len(taskList) == 0 {
+        fmt.Println("No tasks available.")
+        return
+    }
 
-	fmt.Println("Task list:")
-	for _, task := range taskList {
-		status := "❌"
-		if task.Done {
-			status = "✅"
-		}
-		fmt.Printf("%d, [%s], %s\n", task.ID, status, task.Content)
-	}
+    fmt.Println("Task list:")
+    for _, task := range taskList {
+        status := "❌"
+        if task.Done {
+            status = "✅"
+        }
+        fmt.Printf("%d, [%s], %s\n", task.ID, status, task.Content)
+    }
 }
 
 func markTaskAsDone() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: todo done <task id>")
-		return
-	}
+    if len(os.Args) < 3 {
+        fmt.Println("Usage: todo done <task id>")
+        return
+    }
 
-	taskID, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		fmt.Println("Invalid task ID. Pleaes enter a valid number")
-		return
-	}
+    taskID, err := strconv.Atoi(os.Args[2])
+    if err != nil {
+        fmt.Println("Invalid task ID. Pleaes enter a valid number")
+        return
+    }
 
-	updated := false
-	for i, task := range taskList {
-		if task.ID == taskID {
-			if taskList[i].Done {
-				fmt.Printf("Task %d marked as done: %s\n", taskID, task.Content)
-				return
-			}
-			taskList[i].Done = true
-			updated = true
-			break
-		}
-	}
+    updated := false
+    for i, task := range taskList {
+        if task.ID == taskID {
+            if taskList[i].Done {
+                fmt.Printf("Task %d marked as done: %s\n", taskID, task.Content)
+                return
+            }
+            taskList[i].Done = true
+            updated = true
+            break
+        }
+    }
 
-	if updated {
-		saveTasks()
-		fmt.Printf("Task %d marked as done.\n", taskID)
-	} else {
-		fmt.Printf("Task with ID %d not found.\n", taskID)
-	}
+    if updated {
+        saveTasks()
+        fmt.Printf("Task %d marked as done.\n", taskID)
+    } else {
+        fmt.Printf("Task with ID %d not found.\n", taskID)
+    }
 }	
